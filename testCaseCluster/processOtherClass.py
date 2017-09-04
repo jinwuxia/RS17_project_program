b7695e091ba90e61ebb722fe50b180b4959cb347
j123kaishichufa <j123kaishichufa@github.com>
2017-09-05 00:14:14 +0800

    generate otherclass feature vector

--- testCaseCluster/processOtherClass.py
+++ testCaseCluster/processOtherClass.py
@@ -2,23 +2,15 @@
 import sys
 import csv
 
-'''
-算法思路：处理每一个要处理的class， 对其进行分配到已有的cluster或者
-input:已有的cluster结果作为初始值，classID， 重叠的cluster ID。
-读入要处理的class list，以及所在的clusterIDList
-#classList = [classID1, ..]
-#class2ClusterDict[classID] = [clusterID1, clusterID2]
-#假设1:更新过的非Lappcluster与class没有关系
-#假设2:更新过的非Lappcluster也有可能与class有关系
-'''
+#MERGE_FUNC = 'AVG'   #class-cluster depvalue = min,max,avg
+#ASSIGN_THR = 0.03
 
-CONSIDER_FLAG = 'onlyoverlap' # onlyoverlap, all
-MERGE_FUNC = 'AVG'   #class-cluster depvalue = min,max,avg
-ASSIGN_THR = 0.03
+MIN_DEP_VALUE = 0   #usage in feature vector's initialization
 
-FINALCLUSTERDict = dict()  #[clusterID] = classIDList
+#FINALCLUSTERDict = dict()  #[clusterID] = classIDList
 DEP_DICT = dict() #dict[classname1][classname2] = [structdep, commitdep, commudep, mixeddep]
-CLASSID2NAMEDict = dict()
+
+#CLASSID2NAMEDict = dict()
 
 def readDepFile(fileName):
     resDict = dict()
@@ -31,49 +23,16 @@ def readDepFile(fileName):
             resDict[className1][className2] = [round(float(structDep), 5), round(float(commitDep), 5), round(float(communDep), 5), round(float(mixedDep), 5)]
     return resDict
 
-#file=[classID, className, clusterID]
-#return dict[clusterID] = classIDList
-def readClusterFile(fileName):
-    classID2NameDict = dict()
-    clusterDict = dict()
-    with open(fileName, 'rb') as fp:
-        reader = csv.reader(fp)
-        for each in reader:
-            [classID, className, clusterID] = each
-            if classID == 'classID':
-                continue
-            classID2NameDict[int(classID)] = className
-            if int(clusterID) not in clusterDict:
-                clusterDict[int(clusterID)] = list()
-            clusterDict[int(clusterID)].append(int(classID))
-    return classID2NameDict, clusterDict
-
-#file = [classID, className, clusterIDListStr]
-#return dict[classID] = clusterIDList
-def readClassFile(fileName):
-    class2ClusterDict = dict()
-    with open(fileName, 'rb') as fp:
-        reader = csv.reader(fp)
-        for each in reader:
-            [classID, className, clusterIDListStr] = each
-            if classID == 'classID':
-                continue
-            CLASSID2NAMEDict[int(classID)] = className
-            clusterList = clusterIDListStr.split(':')
-            clusterIDList = [int(clusterID)  for clusterID in  clusterList]
-            class2ClusterDict[int(classID)] = clusterIDList
-    return class2ClusterDict
 
 
 #in this function,  the dep[class1, class2] is a single-direction
-def getDepBetClass(classID1, classID2):
-    className1 = CLASSID2NAMEDict[classID1]
-    className2 = CLASSID2NAMEDict[classID2]
+def getDepBetClass(className1, className2):
     if className1 in DEP_DICT:
         if className2 in DEP_DICT[className1]:
             return DEP_DICT[className1][className2][3]   #mixed value
     return round(float(0), 5)
 
+'''
 #dep from classID to clusterID
 def getDep(classID, clusterID):
     resList = list()
@@ -110,110 +69,73 @@ def getDepClassList2Cluster(classList, clusterID):
     if len(newDepValueList)  == 0:
         return -1
     return sum(newDepValueList) / float(len(newDepValueList))
+'''
 
 
-#depList = [clusterID, mixdep][...]
-#decision= extract, multiCopy, singleCopy
-#return resList=[clusterID1, clusterID2].
-#if return =null, extract  it to be a single cluster
-def getClusterDecision(depList):
-    resList = list()
-    for each in depList:
-        [clusterID, depValue] = each
-        if int(depValue) == -1: #assign the class into null cluster at first
-            resList = list()
-            resList.append(clusterID)
-            return resList
-        if depValue > ASSIGN_THR:
-            resList.append(clusterID)
-    return resList
+def readClassFile(fileName):
+    classDict = dict()  #[id] = name
+    with open(fileName, 'rb') as fp:
+        reader = csv.reader(fp)
+        for each in reader:
+            [classID, className] = each
+            if classID == 'classID':
+                continue
+            classDict[className] = 1
+    return classDict
+
+#return allClassDict - hasProClassDict
+def getNotProClasses(allClassDict, hasProClassDict):
+    classList = list()
+    for className in allClassDict:
+        if className not in hasProClassDict:
+            classList.append(className)
+    return classList
 
 
-#judge the two list is equal or not
-def isEqualList(list1, list2):
-    set1 = set(list1)
-    set2 = set(list2)
-    if len(set1 & set2) == len(set1) and len(set1 & set2) == len(set2):
-        return True
-    else:
-        return False
+#single direction dependency
+def genFeatureVector(classList):
+    matrix = list()
+    for i in range(0, len(classList)):
+        tmpList = [MIN_DEP_VALUE] * len(classList)
+        matrix.append(tmpList)
+    for i in range(0, len(classList)):
+        for j in range(0, len(classList)):
+            if i == j:
+                matrix[i][j] = 0
+            else:
+                matrix[i][j] = getDepBetClass(classList[i], classList[j])
+
+    newMatrix = list()
+    for i in range(0, len(classList)):
+        className = classList[i]
+        tmpList = [className]
+        tmpList.extend(matrix[i])
+        newMatrix.append(tmpList)
+    return newMatrix
 
 
-#if merge class into classList, when the classes appears same dependency pattern with clusters
-def preProcess(class2ClusterDict):
-    isProcessed = dict()  #[classID] = 1
-    classSetList = list() #list[1] = classIDs
-    clusterSetList = list() # list[1] = clusterIDs.   classIDs and clusterSetList is corresponding
-
-    classIDKeysList = class2ClusterDict.keys()
-    for index1 in range(0, len(classIDKeysList)):
-        classID1 = classIDKeysList[index1]
-        if classID1 not in isProcessed:
-            classSetList.append([classID1])
-            isProcessed[classID1] = 1
-            clusterIDList1 = class2ClusterDict[classID1]
-            clusterSetList.append(clusterIDList1)
-            for index2 in range(index1 + 1, len(classIDKeysList)):
-                classID2 = classIDKeysList[index2]
-                if classID2 not in isProcessed:
-                    clusterIDList2 = class2ClusterDict[classID2]
-                    if isEqualList(clusterIDList1, clusterIDList2):
-                        classSetList[len(classSetList) - 1].append(classID2)
-                        isProcessed[classID2] = 1
-    print '\nafter merging class: ', classSetList
-    return classSetList, clusterSetList
+def write2CSV(matrix, fileName):
+    with open(fileName, 'wb') as fp:
+        writer = csv.writer(fp)
+        writer.writerows(matrix)
+    print fileName
 
 
-def coreProcess(mergedClassList, mergedClusterList, currentClusterIndex):
-    for index in range(0, len(mergedClassList)):
-        print '\nProcessing: classes=', mergedClassList[index], ' clusters=', mergedClusterList[index]
-        if CONSIDER_FLAG == 'onlyoverlap':  # hypothesis 1
-            clusterList = mergedClusterList[index]
-        elif CONSIDER_FLAG == 'all':        # hypothesis 2
-            clusterList = FINALCLUSTERDict.keys()
-
-        depList = list()
-        for eachClusterID in clusterList:
-            #depList[]=([eachClusterID, mixedValue])
-            mixedDep = getDepClassList2Cluster(mergedClassList[index], eachClusterID)
-            depList.append([eachClusterID, mixedDep])
-
-        #make decision, clusterList = ...
-        print 'before making decision, depList=', depList
-        decisionClusterList = getClusterDecision(depList)
-        print 'after making decision, resList=', decisionClusterList
-
-        if len(decisionClusterList) == 0:  #extract a single cluster
-            FINALCLUSTERDict[currentClusterIndex] = mergedClassList[index]
-            currentClusterIndex += 1
-        else: #multiCopy or singleCopy
-            for assignedClusterID in decisionClusterList:
-                if assignedClusterID not in FINALCLUSTERDict:
-                    FINALCLUSTERDict[assignedClusterID] = list()
-                FINALCLUSTERDict[assignedClusterID].extend(mergedClassList[index])
-
-#pro.py  filterDep.csv   initclusterFile.csv    classListFile.csv
+#pro.py  filterDep.csv   hasProcessedClassFile,  totalClassFile, outFeatureFileName
 if __name__ == '__main__':
     depFileName = sys.argv[1]
-    clusterFileName = sys.argv[2]
-    classListFileName = sys.argv[3]
+    hasProcessedClassFileName = sys.argv[2]
+    totalCLassFileName = sys.argv[3]
+    outFeatureFileName = sys.argv[4]
 
     DEP_DICT = readDepFile(depFileName)
-    [CLASSID2NAMEDict, FINALCLUSTERDict] = readClusterFile(clusterFileName)  #list[clusterID] = classIDList
-    class2ClusterDict = readClassFile(classListFileName) #dict[classID] = clusterIDList
-    print 'class2ClusterDict=',class2ClusterDict
-    tmpList = list()
-    for tmpClassID in class2ClusterDict:
-        tmpClusterList = class2ClusterDict[tmpClassID]
-        tmpList.extend(tmpClusterList)
-    tmpList.extend(FINALCLUSTERDict.keys())
-    currentClusterIndex = 1 + max(tmpList)
-    print 'currentClusterIndex=', currentClusterIndex
-    print 'intiClusters', FINALCLUSTERDict
 
-    [mergedClassList, mergedClusterList]= preProcess(class2ClusterDict)
-    coreProcess(mergedClassList, mergedClusterList, currentClusterIndex)
-    for clusterID in FINALCLUSTERDict:
-        print '\n',clusterID, ':'
-        for classID in FINALCLUSTERDict[clusterID]:
-            print CLASSID2NAMEDict[classID]
+    #get not processed className List, which is our object
+    allClassDict = readClassFile(totalCLassFileName)
+    hasProClassDict = readClassFile(hasProcessedClassFileName)
+    notProClassList = getNotProClasses(allClassDict, hasProClassDict)
+
+    #compute each class's feature vector
+    matrixFv = genFeatureVector(notProClassList)
+    write2CSV(matrixFv, outFeatureFileName)
+    #do clusteirng
