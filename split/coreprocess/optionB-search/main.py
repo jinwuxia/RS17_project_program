ffa78e51acadb9e0824c44ed73c4f2faac206c47
j123kaishichufa <j123kaishichufa@github.com>
2017-09-26 23:41:34 +0800

    add mutation and monitoring record

--- split/coreprocess/optionB-search/main.py
+++ split/coreprocess/optionB-search/main.py
@@ -1,6 +1,4 @@
-
-
-
+OBJECT_STRUCT_DICT = dict()
 
 class ObjectStruct:
     def __init__(self, nonlapClassCount, nonlapClassCount_avg, overlapClassCount, overlapClassCount_avg, \
@@ -25,11 +23,26 @@ class ObjectStruct:
 
 
 #judge the search iteration condition
-def IsStop():
+def IsStop(kgen, old_pop_list, new_pop_list, old_fitness_value_list, new_fitness_value_list):
+    print 'old_pop_list=           ', old_pop_list
+    print 'new_pop_list=           ', new_pop_list
+    print 'old_fitness_value_list= ', old_fitness_value_list
+    print 'new_fitness_value_list= ', new_fitness_value_list
+    if kgen > 10000:
+        return True
+    if len(new_pop_list) == 1:
+        return True
+
+    old_avg = sum(old_fitness_value_list) / len(old_fitness_value_list)
+    new_avg = sum(new_fitness_value_list) / len(new_fitness_value_list)
+    print 'old fitness avg=        ', old_avg
+    print 'new fitness avg=        ', new_avg
+    
+    if kgen > 5000 and abs(old_avg - new_avg) <= 0.00001:
+        return True
     return False
 
-
-def checkChildrenValid(new_pop_list, x_s, x_e, y_s, y_e, bitCount_x, bitCount_y):
+def CheckChildrenValid(new_pop_list, x_s, x_e, y_s, y_e, bitCount_x, bitCount_y):
     import initpop
     invalidLen = 0
     for indiv in new_pop_list:
@@ -37,7 +50,21 @@ def checkChildrenValid(new_pop_list, x_s, x_e, y_s, y_e, bitCount_x, bitCount_y)
             invalidLen += 1
     return invalidLen
 
+
+def GetFitnessList(pop_list, BIT_COUNT_X,  BIT_COUNT_Y):
+    global OBJECT_STRUCT_DICT
+    import initpop
+    fitness_value_list = list()
+    for index  in range(0, len(pop_list)):
+        indiv = pop_list[index]
+        [x, y] = initpop.TransCode2Indiv(indiv, BIT_COUNT_X, BIT_COUNT_Y)  #=[x,y]=[serv, thr_int]
+        fitness_value_list.append(OBJECT_STRUCT_DICT[x][y].withinWorkflow)
+    return fitness_value_list
+
 def MainFunc():
+    global OBJECT_STRUCT_DICT
+    OBJECT_STRUCT_DICT = dict()
+
     #step1: init population
     N = 6 #initial populization size, each generation size = intialSize
     X_S = 2
@@ -49,20 +76,22 @@ def MainFunc():
 
     FITNESSFILENAME = '../../../testcase_data/jpetstore6/coreprocess/jpetstore6-fitness.csv'
 
-    SELECTED_METHOD = 'PRO'  #PRO or SA
+    SELECTED_METHOD = 'SA'  #PRO or SA
     FITNESS_PROBABILITY = 0.10
     TEM = 100
     COOLING_RATE = 0.98
 
     CROSS_OPERATOR = '2P2C'
-    CHILDREN_NUM = 4
+    #CHILDREN_NUM = 4
 
     MUTATION_PROBABILITY = 0.02
+    MUTATION_OPERATOR = 'random' #random  or worse
+
     import fitness
-    objectStructDict = fitness.loadFitness(FITNESSFILENAME)
-    for serv in objectStructDict:
-        for thr_int in objectStructDict[serv]:
-            one = objectStructDict[serv][thr_int]
+    OBJECT_STRUCT_DICT = fitness.loadFitness(FITNESSFILENAME)
+    for serv in OBJECT_STRUCT_DICT:
+        for thr_int in OBJECT_STRUCT_DICT[serv]:
+            one = OBJECT_STRUCT_DICT[serv][thr_int]
             print serv, thr_int, one.nonlapClassCount, one.nonlapClassCount_avg, one.withinWorkflow, one.interWorklow, one.APINum, one.APINum_avg
     print 'loadFitness finished....\n'
 
@@ -78,12 +107,7 @@ def MainFunc():
     while(True):
         print 'loop: ', kgen
         #step2: compute finess
-        fitness_value_list = list()
-        for index  in range(0, len(pop_list)):
-            indiv = pop_list[index]
-            [x, y] = initpop.TransCode2Indiv(indiv, BIT_COUNT_X, BIT_COUNT_Y)  #=[x,y]=[serv, thr_int]
-            fitness_value_list.append(objectStructDict[x][y].withinWorkflow)
-            #print fitness_value_list[index]
+        fitness_value_list = GetFitnessList(pop_list, BIT_COUNT_X, BIT_COUNT_Y)
         print 'fitness_value: ', fitness_value_list
 
         #step3: select better ones as parents for reproduction
@@ -106,31 +130,33 @@ def MainFunc():
         print 'Crossover.....'
         while True:
             new_pop_list = crossover.Crossover([BIT_COUNT_X, BIT_COUNT_Y], selected_pop_list, CROSS_OPERATOR)
-            invalidLen = checkChildrenValid(new_pop_list, X_S, X_E, Y_S, Y_E, BIT_COUNT_X, BIT_COUNT_Y)
+            invalidLen = CheckChildrenValid(new_pop_list, X_S, X_E, Y_S, Y_E, BIT_COUNT_X, BIT_COUNT_Y)
             if invalidLen == 0:
                 print 'After crossover, new_pop_list=', new_pop_list
                 break
             else:
                 print 'invalidLen = ', invalidLen, ';    continue crossover...'
 
-        '''
-        #step5: muatition in a small probability
+
+        #step5: mutation in a small probability
         import mutation
         import random
         mutation_rd = random.random()
         if mutation_rd < MUTATION_PROBABILITY:
             print 'Mutation...'
-            new_pop_list = mutation.Mutation(new_pop_list)
+            tmp_fitness_value_list = GetFitnessList(pop_list, BIT_COUNT_X, BIT_COUNT_Y)
+            #make sure the mutation result is valid
+            new_pop_list = mutation.Mutation(new_pop_list, tmp_fitness_value_list, MUTATION_OPERATOR,\
+                                             X_S, X_E, Y_S, Y_E, BIT_COUNT_X, BIT_COUNT_Y)
             print 'after Mutation, new_pop_list: ', new_pop_list
-        '''
 
+        #old_pop_list  and selected_pop_list are both the parents.
+        if IsStop(kgen, old_pop_list, selected_pop_list, old_fitness_value_list, fitness_value_list):
+            break
         #record states
         old_pop_list = selected_pop_list  #old, parents
         pop_list = new_pop_list  #new, children
         old_fitness_value_list = fitness_value_list  #old
-
-        if IsStop() == True:
-            break
         kgen += 1
 
 MainFunc()
--- split/coreprocess/optionB-search/mutation.py
+++ split/coreprocess/optionB-search/mutation.py
@@ -1,3 +1,26 @@
-def Mutation(new_pop_list):
 
-    return mutation_pop_list
+def MutationOnIndiv(indiv):
+    import random
+    index = random.randint(0, len(indiv) - 1)
+    if indiv[index] == '0':
+        indiv[index] = '1'
+    else:
+        indiv[index] = '0'
+    return indiv
+
+
+def Mutation(pop_list, fitness_value_list, mutation_operator, x_s, x_e, y_s, y_e, bitCount_x, bitCount_y):
+    mutation_indiv_index = -1
+    if mutation_operator == 'random': #randomly choose one indiv to mutate
+        import random
+        mutation_indiv_index = random.randint(0, len(pop_list) - 1)
+    elif mutation_operator == 'worse': #choose the worse one indiv to mutate
+        mutation_indiv_index = fitness_value_list.index( min(fitness_value_list) )
+    else:
+        print 'Unknown mutation operator: ', mutation_operator
+
+    new_indiv  = MutationOnIndiv( pop_list[mutation_indiv_index] )
+    import initpop
+    if initpop.IsValidIndiv(new_indiv, x_s, x_e, y_s, y_e, bitCount_x, bitCount_y):
+        pop_list[mutation_indiv_index] = new_indiv
+    return pop_list
