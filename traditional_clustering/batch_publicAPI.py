d3fb22cef87a23878fa856f04d04267df3e4eee7
j123kaishichufa <j123kaishichufa@github.com>
2017-12-05 14:51:34 +0800

    add parameter

--- traditional_clustering/batch_publicAPI.py
+++ traditional_clustering/batch_publicAPI.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
         api_file_name =     '../testcase_data/jforum219_1/traditional_clustering/jforum219-wca-uem/public_wca_uem_'
         servnum_start = 16
         servnum_end = 47
+        PRE = 'net.jforum.view.forum'
 
     elif project == 'jforum219' and alg == 'limbo':
         testcase_name = ' ../testcase_data/jforum219_1/workflow/jforum219_testcase_name.csv'
@@ -20,6 +21,7 @@ if __name__ == '__main__':
         api_file_name =     '../testcase_data/jforum219_1/traditional_clustering/jforum219-limbo/public_limbo_uem_'
         servnum_start = 16
         servnum_end = 47
+        PRE = 'net.jforum.view.forum'
 
     elif project == 'jpetstore6' and alg == 'wca':
         testcase_name = ' ../testcase_data/jpetstore6/workflow/jpetstore6_testcase_name.csv'
@@ -27,6 +29,7 @@ if __name__ == '__main__':
         api_file_name =     '../testcase_data/jpetstore6/traditional_clustering/jpetstore6-wca-uem/public_wca_uem_'
         servnum_start = 1
         servnum_end = 23
+        PRE = 'org.mybatis.jpetstore.web.actions'
 
     elif project == 'jpetstore6' and alg == 'limbo':
         testcase_name = ' ../testcase_data/jpetstore6/workflow/jpetstore6_testcase_name.csv'
@@ -34,9 +37,10 @@ if __name__ == '__main__':
         api_file_name =     '../testcase_data/jpetstore6/traditional_clustering/jpetstore6-limbo/public_limbo_uem_'
         servnum_start = 1
         servnum_end = 23
+        PRE = 'org.mybatis.jpetstore.web.actions'
 
     for servnum in range(servnum_start, servnum_end + 1):
         this_cluster_file_name = cluster_file_name + str(servnum) + '_clusters.rsf.csv'
         this_api_file_name = api_file_name + str(servnum) + '_clusterAPI.csv'
-        cmd = 'python publicAPI.py  ' + testcase_name  + '  ' +  this_cluster_file_name  + ' ' +  this_api_file_name
+        cmd = 'python publicAPI.py  ' + testcase_name  + '  ' +  this_cluster_file_name  + ' ' +  this_api_file_name + ' ' + PRE
         returncode  = subprocess.call(cmd)
--- traditional_clustering/publicAPI.py
+++ traditional_clustering/publicAPI.py
@@ -3,8 +3,7 @@ import csv
 
 ACTION2APIDict = dict() #[actionClass] = [api1 api2....]
 CLUSTER2CLASSDict = dict() #[clusterID] =[actionclass ]
-PRE = 'org.mybatis.jpetstore.web.actions'
-#PRE = 'net.jforum.view.forum'
+
 def GetAction(api):
     #interface name
     apiList = api.split('.')
@@ -25,13 +24,13 @@ def ReadTsFile(fileName):
             action2APIDict[actionClass].append(apiName)
     return action2APIDict
 
-def IsActionClass(className):
+def IsActionClass(className, PRE):
     if className.startswith(PRE):
         return True
     else:
         return False
 
-def ReadClusterFile(fileName):
+def ReadClusterFile(fileName, PRE):
     cluster2ClassDict = dict()
     with open(fileName, 'rb') as fp:
         reader = csv.reader(fp)
@@ -41,7 +40,7 @@ def ReadClusterFile(fileName):
             clusterID = int(clusterID)
             if clusterID not in cluster2ClassDict:
                 cluster2ClassDict[clusterID] = list()
-            if IsActionClass(className):
+            if IsActionClass(className, PRE):
                 cluster2ClassDict[clusterID].append(className)
     print cluster2ClassDict
     return cluster2ClassDict
@@ -62,16 +61,17 @@ def WriteCSV(listlist, fileName):
         writer.writerows(listlist)
     print fileName
 
-#python pro.py tsFileName clusterFileName apiFileName
+#python pro.py tsFileName clusterFileName apiFileName  PRE
 if __name__ == '__main__':
     tsFileName = sys.argv[1]
     clusterFileName = sys.argv[2]
     apiFileName = sys.argv[3]
+    PRE = sys.argv[4] #PRE = 'org.mybatis.jpetstore.web.actions' #PRE = 'net.jforum.view.forum'
 
     ACTION2APIDict = ReadTsFile(tsFileName)
     #print ACTION2APIDict
 
-    CLUSTER2CLASSDict = ReadClusterFile(clusterFileName)
+    CLUSTER2CLASSDict = ReadClusterFile(clusterFileName, PRE)
     #print CLUSTER2CLASSDict
 
     apiList = ExtractAPI()
