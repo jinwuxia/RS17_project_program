ddb00795268a4e1374c6eb1e12cb38638805933d
j123kaishichufa <j123kaishichufa@github.com>
2017-12-15 18:16:39 +0800

    get rsf from und

---
+++ traditional_clustering/getRSF.py
@@ -0,0 +1,30 @@
+import sys
+import csv
+
+def readCSV(fileName, filterStr):
+    depsList = list()
+    with open(fileName, 'rb') as fp:
+        reader = csv.reader(fp, delimiter = ',')
+        for eachLine in reader:
+            [fromClass, toClass, weight, fromNum, toNum] = eachLine
+            if fromClass == "From Class":
+                continue
+
+            if fromClass.startswith(filterStr) == True and toClass.startswith(filterStr) == True:
+                depsList.append(['depends', fromClass, toClass])
+    return depsList
+
+
+def writersf(fileName, depsList):
+    with open(fileName, 'wb') as fp:
+        writer = csv.writer(fp, delimiter=' ')
+        writer.writerows(depsList)
+    fp.close()
+
+#input und.csv; output  class_dep.rsf  class_dep.csv 
+if __name__ == "__main__":
+    undDepName = sys.argv[1]
+    rsfName = sys.argv[2]
+    filterStr = sys.argv[3]
+    rsfList = readCSV(undDepName, filterStr)
+    writersf(rsfName, rsfList)
