cb0ce261816e37f0e06a9e5121ed86a4c6b66212
j123kaishichufa <j123kaishichufa@github.com>
2017-12-17 18:26:40 +0800

    modify to analyze roller

--- traditional_clustering/batch_analyzeClusterAndAPI.py
+++ traditional_clustering/batch_analyzeClusterAndAPI.py
@@ -1,36 +1,27 @@
 import os
 import subprocess
+import sys
+
 if __name__ == '__main__':
+    project = sys.argv[1]  #jpetstore6, jforum219, roller520
+    alg = sys.argv[2]      #wca, limbo
+    cluster_start = int(sys.argv[3])   #cluster_num_min
+    cluster_end = int(sys.argv[4])   #cluster_num_max
 
-    '''
-    #jforum219   limbo
-    cluster_file_name = '../testcase_data/jforum219_1/traditional_clustering/jforum219-limbo/jwx_limbo_ilm_LIMBO_preselected_ilm_'
-    workflow_file_name = '../testcase_data/jforum219_1/workflow/jforum219_workflow_reduced.csv'
-    api_file_name = '../testcase_data/jforum219_1/traditional_clustering/jforum219-limbo/limbo_'
-    '''
-    '''
-    #jforum219   wca uem
-    cluster_file_name = '../testcase_data/jforum219_1/traditional_clustering/jforum219-wca-uem/jwx_wca_uem_WCA_preselected_uem_'
-    workflow_file_name = '../testcase_data/jforum219_1/workflow/jforum219_workflow_reduced.csv'
-    api_file_name = '../testcase_data/jforum219_1/traditional_clustering/jforum219-wca-uem/wca_'
-    '''
-    '''
-    #jpetstore6   limbo
-    cluster_file_name = '../testcase_data/jpetstore6/traditional_clustering/jpetstore6-limbo/jwx_limbo_ilm_LIMBO_preselected_ilm_'
-    workflow_file_name = '../testcase_data/jpetstore6/workflow/jpetstore6_workflow_reduced.csv'
-    api_file_name = '../testcase_data/jpetstore6/traditional_clustering/jpetstore6-limbo/limbo_'
-    '''
+    workflow_file_name = '../testcase_data/' + project + '/workflow/' + project + '_workflow_reduced.csv'
+    if alg == 'wca':
+        cluster_file_name = ('../testcase_data/' + project + '/traditional_clustering/wca/clusters/jwx_wca_uem_WCA_preselected_uem_')
+        api_file_name     = ('../testcase_data/' + project + '/traditional_clustering/wca/clusterAPI/' + project + '_wca_')
+    elif alg == 'limbo':
+        cluster_file_name = ('../testcase_data/' + project + '/traditional_clustering/limbo/clusters/jwx_limbo_ilm_LIMBO_preselected_ilm_')
+        api_file_name     = ('../testcase_data/' + project + '/traditional_clustering/limbo/clusterAPI/' + project + '_limbo_')
+    else:
+        print 'unknown ' + alg
 
-
-    #jpetstore6 wca
-    cluster_file_name = '../testcase_data/jpetstore6/traditional_clustering/jpetstore6-wca-uem/jwx_wca_uem_WCA_preselected_uem_'
-    workflow_file_name = '../testcase_data/jpetstore6/workflow/jpetstore6_workflow_reduced.csv'
-    api_file_name = '../testcase_data/jpetstore6/traditional_clustering/jpetstore6-wca-uem/wca_'
-
-    #for servnum in range(16,48):
-    for servnum in range(1,24):
+    for servnum in range(cluster_start, cluster_end + 1):
         this_cluster_file_name = cluster_file_name + str(servnum) + '_clusters.rsf.csv'
         this_api_file_name = api_file_name + str(servnum) + '_clusterAPI.csv'
         cmd = 'python  analyzeClusterAndAPI.py  ' +  this_cluster_file_name + '  ' +   workflow_file_name + '  '  +  this_api_file_name
-        returncode  = subprocess.call(cmd)
+        #print cmd
+        returncode  = subprocess.call(cmd, shell=True)
         #print returncode
