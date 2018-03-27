9fd1e4c96701c677b194149f9885efa01a4e6503
j123kaishichufa <j123kaishichufa@163.com>
2018-03-27 15:05:02 -0400

    add bvn13 abd solo13 batch process

--- split/coreprocess/batch_analyzeCluster.sh
+++ split/coreprocess/batch_analyzeCluster.sh
@@ -19,10 +19,35 @@ python coreprocess/analyzeAllCluster.py   $file    ../testcase_data/jforum219_1/
 done
 eof
 
+
+:<<eof
 #roller
 project=roller520
 cluster_num=72
 for((i=${cluster_num};i>=7;i--));
+do
+file='../testcase_data/'${project}'/coreprocess/testcaseClustering/'${project}'_testcase1_jm_AVG_'${i}'.csv'
+python coreprocess/analyzeAllCluster.py   $file    ../testcase_data/${project}/coreprocess/${project}_testcase1_fv.csv    ../testcase_data/${project}/coreprocess/${project}_testcase1_class.csv
+#mv 'comb_tmp.txt'   'comb_tmp_'${i}'.txt'
+done
+eof
+
+:<<eof
+#bvn13
+project=bvn13
+cluster_num=26
+for((i=${cluster_num};i>=1;i--));
+do
+file='../testcase_data/'${project}'/coreprocess/testcaseClustering/'${project}'_testcase1_jm_AVG_'${i}'.csv'
+python coreprocess/analyzeAllCluster.py   $file    ../testcase_data/${project}/coreprocess/${project}_testcase1_fv.csv    ../testcase_data/${project}/coreprocess/${project}_testcase1_class.csv
+#mv 'comb_tmp.txt'   'comb_tmp_'${i}'.txt'
+done
+eof
+
+#solo270
+project=solo270
+cluster_num=70
+for((i=${cluster_num};i>=2;i--));
 do
 file='../testcase_data/'${project}'/coreprocess/testcaseClustering/'${project}'_testcase1_jm_AVG_'${i}'.csv'
 python coreprocess/analyzeAllCluster.py   $file    ../testcase_data/${project}/coreprocess/${project}_testcase1_fv.csv    ../testcase_data/${project}/coreprocess/${project}_testcase1_class.csv
