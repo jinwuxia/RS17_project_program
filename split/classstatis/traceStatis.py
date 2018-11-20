64c1d2a631af7543d97224c9582c47588d673a48
j123kaishichufa <j123kaishichufa@163.com>
2018-11-19 21:32:06 -0500

    read workflow by adding additional column: weight

--- split/classstatis/traceStatis.py
+++ split/classstatis/traceStatis.py
@@ -2,16 +2,26 @@ import sys
 import csv
 
 
+def isNewTrace(oldTraceId, currentTraceId):
+    if oldTraceId == -1:
+        return True
+    if oldTraceId != -1 and oldTraceId != currentTraceId:
+        return True
+    return False
+    
 def readCSV(fileName):
     resList = list()
-    with open(fileName, "rb") as fp:
+    with open(fileName, "r", newline="") as fp:
         reader = csv.reader(fp)
+        oldTraceId = -1
         for each in reader:
-            [traceID, order, structtype, method1, method2, m1_para, m2_para, className1, className2, m1_return, m2_return] = each
+            [traceID, order, structtype, method1, method2, m1_para, m2_para, className1, className2, m1_return, m2_return, weight] = each
             if traceID == 'traceID':
                 continue
-            if order == '0':
+            #if order == '0':
+            if isNewTrace(oldTraceId, traceID):
                 resList.append(list())
+                oldTraceId = traceID
             oneList = [method1, method2, className1, className2]
             resList[int(traceID)].append(oneList)
     return resList
@@ -38,10 +48,10 @@ def statis(traceList):
 
     for method1 in edgeDict:
         uniqueMethodCall += len(edgeDict[method1].keys())
-    print 'traceCount', len(traceList)
-    print 'methodCount', len(methodList)
-    print 'methodCall', methodCall
-    print 'uniqueMethodCall', uniqueMethodCall
+    print ('traceCount', len(traceList))
+    print ('methodCount', len(methodList))
+    print ('methodCall', methodCall)
+    print ('uniqueMethodCall', uniqueMethodCall)
 
 
 
